8524a1530cd3971afd174a3afe84585e999b7f9a
sara-patino <sara.patino@alumnos.uneatlantico.es>
2022-06-15 12:40:12 +0200

    se agregaron los demás fantasmas y ahora el Pacman come

--- sara.patino/pacman.java
+++ sara.patino/pacman.java
@@ -1,136 +1,167 @@
 import java.util.Scanner;
-class pacman{	
-
-	private static int posicionX, posicionY;
-	private static int posicionNPCX, posicionNPCY;
-
-
+class Pacman{	
+	
 	public static void main(String[] args){
-
+			
 		int[][] unMapa = {
-					    {1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
-                        {1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1,	1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	2,  1,	1,	1,	1,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	4,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	4,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	2,	2,	2,	2,	2,	1,	2,	2,	2,	2,	2,	1,	1,	2,	2,	2,	2,	2,	1,	2,	2,	2,	2,	2,	2,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	0,	1,	1,	0,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	0,	1,	1,	0,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	1,	1,	1,	1,	0,	0,	1,	1,	1,	1,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {0,	0,	0,	0,	0,	0,	2,	0,	0,	1,	0,	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	2,	0,	0,	0,	0,	0,	0},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	0,	0,	0,	0,	6,	6,	0,	0,	0,	0,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	1,	1,	1,	1,	1,	2,	1,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	1,	2,	1,	1,	1,	1,	1,	1},
-                        {1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1,	1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	2,	2,	2,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	2,	2,	2,	1,	1,	1,	1,	2,	1},
-                        {1,	4,	2,	2,	1,	1,	2,	1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1,	2,	1,	1,	2,	2,	4,	1},
-                        {1,	1,	1,	2,	1,	1,	2,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	2,	1,	1,	2,	1,	1,	1},
-                        {1,	1,	1,	2,	1,	1,	2,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	2,	1,	1,	2,	1,	1,	1},
-                        {1,	2,	2,	2,	2,	2,	2,	1,	2,	2,	2,	2,	2,	1,	1,	2,	2,	2,	2,	2,	1,	2,	2,	2,	2,	2,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1,	1,	2,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	2,	1},
-                        {1,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	2,	1},
-                        {1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}
-		};
+            {1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
+            {1, 2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 4,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  4,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 2,  2,  2,  2,  2,  2,  1,  2,  2,  2,  2,  2,  1,  1,  2,  2,  2,  2,  2,  1,  2,  2,  2,  2,  2,  2,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  0,  1,  1,  0,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  0,  1,  1,  0,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  1,  1,  1,  1,  0,  0,  1,  1,  1,  1,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {0, 0,  0,  0,  0,  0,  2,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  2,  0,  0,  0,  0,  0,  0},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  0,  0,  0,  0,  6,  6,  0,  0,  0,  0,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 1,  1,  1,  1,  1,  2,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  2,  1,  1,  1,  1,  1,  1},
+            {1, 2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  2,  1},
+            {1, 2,  1,  1,  1,  1,  2,  2,  2,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  2,  2,  2,  1,  1,  1,  1,  2,  1},
+            {1, 4,  2,  2,  1,  1,  2,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  2,  1,  1,  2,  2,  4,  1},
+            {1, 1,  1,  2,  1,  1,  2,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  2,  1,  1,  2,  1,  1,  1},
+            {1, 1,  1,  2,  1,  1,  2,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  2,  1,  1,  2,  1,  1,  1},
+            {1, 2,  2,  2,  2,  2,  2,  1,  2,  2,  2,  2,  2,  1,  1,  2,  2,  2,  2,  2,  1,  2,  2,  2,  2,  2,  2,  1},
+            {1, 2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1},
+            {1, 2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  1},
+            {1, 2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1},
+            {1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}
 
-		posicionX=2; 
-		posicionY=2;
-		posicionNPCX = 7;
-		posicionNPCY = 5;
+        };
 
+		int[][] elPersonaje = { { 1, 1 } };
+        int[][] losNPCs = {
+            { 12, 14 },
+            { 12, 12 },
+            { 14, 14 },
+            { 14, 12 },
+    };
 		do {
-			imprimeMapa(unMapa);
-		} while (procesaMovimiento(unMapa));
+			imprimeMapa(unMapa, elPersonaje, losNPCs);
+		} while (procesaMovimiento(unMapa,elPersonaje, losNPCs));
 	}
-
-	private static boolean procesaMovimiento(int[][] elMapa){
-
+	private static boolean procesaMovimiento(int[][] elMapa, int[][]elPersonaje, int[][] losNPCs){
+        mueveNPCs(elMapa, losNPCs);
 		Scanner entrada = new Scanner(System.in);
 		String inputUsuario;
-
+		
 		inputUsuario = entrada.nextLine();
+        int elPersonajeX, elPersonajeY;
+        
+		
+        elPersonajeX = elPersonaje[0][0];
+		elPersonajeY = elPersonaje[0][1];
+		    if (inputUsuario.equals("a") && elMapa[elPersonajeY][elPersonajeX - 1] % 2 == 0) {
+			elPersonajeX = elPersonajeX - 1;
+		    } else if (inputUsuario.equals("d") && elMapa[elPersonajeY][elPersonajeX + 1] % 2 == 0) {
+			elPersonajeX = elPersonajeX + 1;
+		    } else if (inputUsuario.equals("w") && elMapa[elPersonajeY - 1][elPersonajeX] % 2 == 0) {
+			elPersonajeY = elPersonajeY - 1;
+		    } else if (inputUsuario.equals("s") && elMapa[elPersonajeY + 1][elPersonajeX] % 2 == 0) {
+			elPersonajeY = elPersonajeY + 1;
+	    	} else if (inputUsuario.equals("f")) {
+			        return false;
+		}
 
-		elMapa[posicionY][posicionX]=2;
-		mueveNPCs(elMapa);
 
+        elPersonaje[0][0] = elPersonajeX;
+		elPersonaje[0][1] = elPersonajeY;
 
-		if (inputUsuario.equals("a") && elMapa[posicionY][posicionX-1]%2==0) {posicionX=posicionX-1;} else 
-		if (inputUsuario.equals("d") && elMapa[posicionY][posicionX+1]%2==0) {posicionX=posicionX+1;} else 
-		if (inputUsuario.equals("w") && elMapa[posicionY-1][posicionX]%2==0) {posicionY=posicionY-1;} else 
-		if (inputUsuario.equals("s") && elMapa[posicionY+1][posicionX]%2==0) {posicionY=posicionY+1;} else 
-		if (inputUsuario.equals("f")) { return false;}
+        if(elMapa[elPersonajeY][elPersonajeX]==2){
+            elMapa[elPersonajeY][elPersonajeX]=0;
+        } 
 
 		return true;
 	}
 
-	private static void mueveNPCs(int[][] elMapa) {
-		double movimiento = Math.random();
-
-		if (movimiento<=0.25 && elMapa[posicionNPCY][posicionNPCX-1]%2==0) {posicionNPCX=posicionNPCX-1;} else 
-		if (movimiento<=0.5 && elMapa[posicionNPCY][posicionNPCX+1]%2==0) {posicionNPCX=posicionNPCX+1;} else 
-		if (movimiento<=0.75 && elMapa[posicionNPCY-1][posicionNPCX]%2==0) {posicionNPCY=posicionNPCY-1;} else 
-		if (movimiento<=1 && elMapa[posicionNPCY+1][posicionNPCX]%2==0) {posicionNPCY=posicionNPCY+1;}
-
+	private static void mueveNPCs(int[][] elMapa, int[][] losNPCs) {
+		double movimiento;
+		int npcX, npcY;
+		for (int unNPC = 0; unNPC < losNPCs.length; unNPC++) {
+			movimiento = Math.random();
+			npcX = losNPCs[unNPC][0];
+			npcY = losNPCs[unNPC][1];
+			if (movimiento <= 0.25 && elMapa[npcY][npcX - 1] % 2 == 0) {
+				npcX = npcX - 1;
+			} else if (movimiento <= 0.5 && elMapa[npcY][npcX + 1] % 2 == 0) {
+				npcX = npcX + 1;
+			} else if (movimiento <= 0.75 && elMapa[npcY - 1][npcX] % 2 == 0) {
+				npcY = npcY - 1;
+			} else if (movimiento <= 1 && elMapa[npcY + 1][npcX] % 2 == 0) {
+				npcY = npcY + 1;
+			}
+			losNPCs[unNPC][0] = npcX;
+			losNPCs[unNPC][1] = npcY;
+		}
 	}
-
+    private static void imprimeMapa(int[][] mapaPorImprimir, int[][]elPersonaje, int[][] losNPCs){
+		
+		imprimeBordeHorizontal(mapaPorImprimir[0].length);
+		
+		for (int i=0; i<mapaPorImprimir.length; i=i+1){
+			imprimeBordeVertical(false);
+			for (int j=0; j<mapaPorImprimir[i].length; j=j+1) {
+					if (i==elPersonaje[0][1] && j==elPersonaje[0][0]) {
+						imprimePersonaje();
+					}else {
+						if (hayNPC(losNPCs, i, j)) {
+							imprimeNPC();
+						} else {
+							imprimeElemento(mapaPorImprimir[i][j]);
+						}
+					}
+				 
+			}
+			imprimeBordeVertical(true);
+		}		
+		imprimeBordeHorizontal(mapaPorImprimir[0].length);
+		imprimeStatus(elPersonaje, losNPCs);
+	}
+	private static void imprimeStatus(int[][] elPersonaje, int[][] losNPCs) {
+		System.out.println("Personaje en X:[" + elPersonaje[0][0] + "] Y:[" + elPersonaje[0][1] + "]");
+		for (int unNPC = 0; unNPC < losNPCs.length; unNPC++) {
+			System.out.print("NPC[" + unNPC + "]=(" + losNPCs[unNPC][0] + "," + losNPCs[unNPC][1] + ") - ");
+		}
+	}
+	
 	private static void imprimePersonaje(){
 		System.out.print(" :v");
 	}
-
 	private static void imprimeBordeHorizontal(int laLongitud){
-
 		System.out.print("+");
 		for (int j=0;j<laLongitud;j=j+1){
 			System.out.print("---");
 		}
 		System.out.println("+");		
 	}
-
 	private static void imprimeBordeVertical(boolean bordeDerecho){
 		System.out.print("|");
 		if (bordeDerecho) {System.out.println();}
 	}
-
-	private static void imprimeMapa(int[][] mapaPorImprimir){
-
-		imprimeBordeHorizontal(mapaPorImprimir[0].length);
-
-		for (int i=0; i<mapaPorImprimir.length; i=i+1){
-			imprimeBordeVertical(false);
-			for (int j=0; j<mapaPorImprimir[i].length; j=j+1) {
-					if (i==posicionY && j==posicionX) {
-						imprimePersonaje();
-					} else if (i==posicionNPCY && j==posicionNPCX) {
-						imprimeNPC();
-					} else {
-						imprimeElemento(mapaPorImprimir[i][j]);
-					}
-
+	
+	
+	private static boolean hayNPC(int[][] losNPCs, int i, int j) {
+		for (int unNPC = 0; unNPC < losNPCs.length; unNPC++) {
+			if (losNPCs[unNPC][0] == j && losNPCs[unNPC][1] == i) {
+				return true;
 			}
-			imprimeBordeVertical(true);
-		}		
-		imprimeBordeHorizontal(mapaPorImprimir[0].length);
-
-		System.out.println("Personaje en X:["+posicionX+"] Y:["+posicionY+"]");
+		}
+		return false;
 	}
-
+	
 	private static void imprimeNPC() {
-
 		System.out.print("^V^");
-
 	}
-
-
 	private static void imprimeElemento(int elementoDelMapa) {
-		String[] matrizDeElementos = {" . ","[#]","---","333"," * ","555"," @ ","777","   "};
-
+		String[] matrizDeElementos = {"   ","[#]"," . ","333"," * ","555"," @ "};
 		System.out.print(matrizDeElementos[elementoDelMapa]);
 	}
-
+	
 }
